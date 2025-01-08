f34e1586778d546ff4e851a99429ed6d5333a094
dionnes <dionne.simms@gmail.com>
2025-01-08 21:46:40 +0530

    create ec2

--- ds-ec2/lib/ds-ec2-stack.ts
+++ ds-ec2/lib/ds-ec2-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DsEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // define default vpc to use from exsting region
+    const vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC', {
+      isDefault: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DsEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create new ec2 instance
+    const dsec2inst = new ec2.Instance(this, 'dsmachine', {
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      // instanceName: 'dsec2inst',
+      keyName: 'splunk-key'
+    });
+
   }
 }
